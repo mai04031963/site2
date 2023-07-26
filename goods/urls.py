6fa19035638d4409f70ed29eca4596575151a9fb
mai04031963 <136967136+mai04031963@users.noreply.github.com>
2023-07-26 16:49:07 +0300

    Add files via upload

--- goods/urls.py
+++ goods/urls.py
@@ -1,12 +1,15 @@
-from django.urls import path
-from . views import GoodsListView, goods_list
+from django.urls import path, re_path
+from . views import good_search2, index
 
 urlpatterns = [
-    path("", goods_list, name="index"),
-    # ex: /goods/5/
-    #path("<int:_id>/", views.detail, name="detail"),
-    # ex: /goods/5/results/
-    #path("<int:_id>/results/", views.results, name="results"),
-    # ex: /goods/5/vote/
-    #path("<int:_id>/vote/", views.vote, name="vote")
+    path("", good_search2, name='catalogs'),
+    path("<int:_id1>/", good_search2, name='catalogs1'),
+    path("<int:_id1>/<int:_id2>/", good_search2, name='catalogs2'),
+    path("<int:_id1>/<int:_id2>/<int:_id3>/", good_search2, name='catalogs3'),
+    path("index<int:_id>/", index, name='index'),
+    path("<int:_id1>/index<int:_id>/", index, name='index1'),
+    path("<int:_id1>/<int:_id2>/index<int:_id>/", index, name='index2'),
+    path("<int:_id1>/<int:_id2>/<int:_id3>/index<int:_id>/", index, name='index3'),
 ]
+
+
