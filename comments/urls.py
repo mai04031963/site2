d20494b2de507bb1589f7cc8982b11ed46629897
mai04031963 <136967136+mai04031963@users.noreply.github.com>
2023-08-01 14:49:09 +0300

    Delete comments directory

--- comments/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.CommentsListView.as_view(), name="comments"),
-    path('new_comment/', views.new_comment2, name="new_comment")
-    ]
