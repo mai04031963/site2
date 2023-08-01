dff99e94171a55f97216d37d4ab607fe4c77fd79
mai04031963 <136967136+mai04031963@users.noreply.github.com>
2023-08-01 14:48:37 +0300

    Delete service directory

--- service/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('new_demand/',views.new_demand, name="new_demand"),
-    path('', views.service, name="comments")
-    ]
